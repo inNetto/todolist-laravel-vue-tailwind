e9d6e58bf43dedfc60e32abda80cba373529461f
IvanNetto <inetto.ferreira@gmail.com>
2022-02-03 20:10:05 -0300

    exceptions and user registration

--- app/Services/AuthService.php
+++ app/Services/AuthService.php
@@ -4,7 +4,11 @@
 namespace App\Services;
 
 
+use App\Events\UserRegistered;
 use App\Exceptions\LoginInvalidException;
+use App\Exceptions\UserHasBeenTakenException;
+use App\Models\User;
+use Illuminate\Support\Str;
 
 class AuthService
 {
@@ -29,5 +33,35 @@ class AuthService
             'access_token' => $token,
             'token_type' => 'Bearer'
         ];
+    }
+
+    /**
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $email
+     * @param string $password
+     * @return mixed
+     * @throws UserHasBeenTakenException
+     */
+    public function register(string $firstName, string $lastName, string $email, string $password)
+    {
+        $user = User::where('email', $email)->exists();
+        if (!empty($user)) {
+            throw new UserHasBeenTakenException();
+        }
+
+        $userPassword = bcrypt($password ?? Str::random(10));
+
+        $user = User::create([
+            'first_name'         => $firstName,
+            'last_name'          => $lastName,
+            'email'              => $email,
+            'password'           => $userPassword,
+            'confirmation_token' => Str::random(60)
+        ]);
+
+        event(new UserRegistered($user));
+
+        return $user;
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
-Route::post('login',[\App\Http\Controllers\AuthController::class,'login']);
+Route::prefix('v1')->group(function() {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+});
